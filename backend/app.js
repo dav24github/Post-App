06fae4d8f000d99f45eedc6404d1f5fa22633ba3
dav24github <Davidthbsossa@gmail.com>
2022-02-01 18:40:21 -0400

    make it better

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose
 
 app.use(express.json());
 
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join(process.cwd(), "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
